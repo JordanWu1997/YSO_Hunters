9034aa700ea19e52f9e62451f2b4ce74a3816778
JordanWu <swingclouds@gmail.com>
2019-10-20 22:05:35 +0800

    storage_for_all_path_needed_for_SOP_Programs

---
+++ SOP_Program_Path/SOP_Program_Path.py
@@ -0,0 +1,22 @@
+
+# SOP_Program_Preset/SOP_Execution_Preset.py
+Hsieh_Av_table = "/home/ken/C2D-SWIRE_20180710/All_Extinction_Table/Tables_From_Hsieh/"
+Selfmade_Av_table = "/home/ken/C2D-SWIRE_20180710/All_Extinction_Table/Tables_Self_Made/"
+
+# SOP_Program_Preset/Find_Saturate.py
+# SOP_Program_5D_method/getfits.py
+Mosaic_path = "/data/public/spitzer/c2d/data.spitzer.caltech.edu/popular/c2d/20071101_enhanced_v1/"
+
+# SOP_Program_5D_method/multi-d_Prob_J_MP1.py
+# SOP_Program_5D_method/PSF1_detection_multi-d_Prob_J_MP1.py
+Hsieh_5D_GP1_Array_path = "/cosmo/users/inchone/Remove_Av_sources_in_whole_clouds/SWI_c2d_PSD_new_process2_multi-dim_version/J_MP1_plus_IR1_MP1_methed/c2d_SWIRE_J-MP1/"
+
+# SOP_Program_5D_method/multi-d_Prob_IR1_MP1.py
+# SOP_Program_5D_method/PSF1_detection_multi-d_Prob_IR1_MP1.py
+Hsieh_5D_GP2_Array_path = "/cosmo/users/inchone/Remove_Av_sources_in_whole_clouds/SWI_c2d_PSD_new_process2_multi-dim_version/J_MP1_plus_IR1_MP1_methed/c2d_SWIRE_IR1-MP1/"
+
+# SOP_Program_6D_method/new_dict_6D_method.py
+Selfmade_6D_GP_Dict_path = "/home/ken/new_mg/GPV_SOP_Program/"
+
+# SOP_Program_6D_method/SOP_Execution_6D_method.py
+Hsieh_YSO_List_path = '/home/ken/C2D-SWIRE_20180710/Table_to_Compare/Table_From_Hsieh/'
