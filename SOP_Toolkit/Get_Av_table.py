4959eb745de93b83acce2075dd3de2c6e0b36c6a
JordanWu <swingclouds@gmail.com>
2019-10-22 15:16:33 +0800

    program_to_get_value_from_extinction_map

---
+++ SOP_Toolkit/Get_Av_table.py
@@ -0,0 +1,40 @@
+#!/usr/bin/ipython
+'''
+#==================================
+
+This Program is from Jacob and revised by Jordan.
+
+#==================================
+Jordan Wu 20190806'''
+
+import numpy as np
+from astropy.io import fits as pyfits
+from astropy import wcs
+from sys import argv
+
+image = str(argv[1])
+output = str(argv[2])
+
+hdu = pyfits.open(image)
+header = hdu[0].header
+emap = hdu[0].data
+
+# reshape data
+x_pix = range(emap.shape[1])
+y_pix = range(emap.shape[0])
+xv_pix, yv_pix = np.meshgrid(x_pix, y_pix)
+xv_pix = xv_pix.flatten()
+yv_pix = yv_pix.flatten()
+emap = emap.flatten()
+
+# Convert pixel to wcs
+w = wcs.WCS(header)
+pixcrd = np.array([xv_pix, yv_pix])
+pixcrd = np.transpose(pixcrd)
+world = w.wcs_pix2world(pixcrd, 1)
+world = np.transpose(world)
+emap_table = np.array([world[0], world[1], emap])
+emap_table = np.transpose(emap_table)
+
+# Save Av and position into table.
+np.savetxt(output, emap_table, fmt="%1.4f")
