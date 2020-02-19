2cbe7f4066cad4df1d03bff1a326aeb1150ff1a2
JordanWu <swingclouds@gmail.com>
2020-02-19 23:27:17 +0800

    add_timer

--- SOP_00_Gal_Prob/Sort_Source_Lack999.py
+++ SOP_00_Gal_Prob/Sort_Source_Lack999.py
@@ -13,15 +13,16 @@ if len(argv) != 3:
     \n\t[dim]: dim of magnitude space (for now only "6")\
     \n\t[cube size]: length of multi-d cube in magnitude unit\n')
 
-print("Loading ...")
+print("\nLoading ...\n")
 dim      = int(argv[1])       # Dimension of multi-D method
-lack_lim = dim - 3
+lack_lim = dim - 3 + 1        # Note: start from 0
 cube     = float(argv[2])     # Beamsize for each cube
 posv_dir = 'GPV_' + str(dim) + 'Dposvec_bin' + str(cube) + '/'
 shape    = list(np.load(posv_dir + "Shape.npy"))
 gal_pos  = np.load(posv_dir + "Gal_Position_vectors.npy")
 gal_num  = np.load(posv_dir + "Gal_Position_numbers.npy")
 
+#=======================================================
 # Functions
 def drawProgressBar(percent, barLen = 50):
     # percent float from 0 to 1.
@@ -29,7 +30,9 @@ def drawProgressBar(percent, barLen = 50):
     sys.stdout.write("[{:<{}}] {:.3f}%".format("=" * int(barLen * percent), barLen, (percent * 100)))
     sys.stdout.flush()
 
+#=======================================================
 # Sort
+w_start = time.time()
 for i in range(lack_lim):
     lack_pos = []
     lack_num = []
@@ -47,4 +50,6 @@ for i in range(lack_lim):
     np.save('Lack_{:d}{:d}_num'.format(i, i), lack_num_array)
     chdir('../')
     s_end     = time.time()
-    print('\nFilter out lack {:d} band took {:.3f}\n'.format(i, s_end-s_start))
+    print('\nFilter out lack {:d} band took {:.3f} sec\n'.format(i, s_end-s_start))
+w_end   = time.time()
+print('\nWhole sorting process took {:.3f} sec\n'.format(w_end-w_start))
