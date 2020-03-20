47942c6a1735022da84f66132c4c765527a0853a
JordanWu <swingclouds@gmail.com>
2020-03-20 12:52:56 +0800

    generalize_to_all_band

--- SOP_00_Gal_Prob/Count_Gal_Pos_Vec_numba.py
+++ SOP_00_Gal_Prob/Count_Gal_Pos_Vec_numba.py
@@ -7,13 +7,15 @@ from numba import jit
 from sys import argv, exit, stdout
 from os import chdir, system, path
 from Hsieh_Functions import *
+from Useful_Functions import *
 
-#TODO Add input bands
-if len(argv) != 5:
+#TODO Add input bands in the future
+if len(argv) != 6:
     exit('\n\tError: Wrong Arguments\
-    \n\tExample: [program] [input catalog] [mag/flux] [dimension] [cube size]\
+    \n\tExample: [program] [input catalog] [mag/flux] [qua] [dimension] [cube size]\
     \n\t[input catalog]: must include magnitudes\
     \n\t[mag/flux]: input data in magnitude or flux (mJy)\
+    \n\t[qua]: if qua label is taken into calculation\
     \n\t[dimension]: dim of magnitude space (for now only "6")\
     \n\t[cube size]: length of multi-d cube in magnitude unit\n')
 
@@ -21,43 +23,36 @@ if len(argv) != 5:
 # Set parameters
 inpcat   = str(argv[1])
 datatype = str(argv[2])
-dim      = int(argv[3])
-cube     = float(argv[4])
-print('\ncube = ' + str(cube))
+qualabel = bool(argv[3] == 'True')
+dim      = int(argv[4])
+cube     = float(argv[5])
 
 # J, IR1, IR2, IR3, IR4, MP1
 # If input spizer catalog, just comment below 2 lines
-#flux_ID = [0, 3, 4, 5, 6, 7]
-#mag_ID  = [0, 3, 4, 5, 6, 7]
-
-print('flux_ID')
-print(flux_ID)
-print('mag_ID')
-print(mag_ID)
-
-#TODO Generalize this part to arbitary dimensions
-#======================================================================
+flux_ID = [0, 3, 4, 5, 6, 7]
+mag_ID  = [0, 3, 4, 5, 6, 7]
+band_ID = [0, 3, 4, 5, 6, 7]
+# JHK photometry system
+JHK_system = 'ukidss' #'2mass'
 # Use Limit Stored in Hsieh_Functions
 Jaxlim   = Hsieh_Jaxlim
 Ksaxlim  = Hsieh_Ksaxlim
+Haxlim   = [0, 0]         # Not Mentioned in Hsieh
 IR1axlim = Hsieh_IR1axlim
 IR2axlim = Hsieh_IR2axlim
 IR3axlim = Hsieh_IR3axlim
 IR4axlim = Hsieh_IR4axlim
 MP1axlim = Hsieh_MP1axlim
-# For now, only 6 band
-axlim_list = [Jaxlim, IR1axlim, IR2axlim, IR3axlim, IR4axlim, MP1axlim]
-# Count shape of each dim
-binsa = int(round((  Jaxlim[1] -   Jaxlim[0]) / cube)) + 1
-binsb = int(round(( Ksaxlim[1] -  Ksaxlim[0]) / cube)) + 1
-bins1 = int(round((IR1axlim[1] - IR1axlim[0]) / cube)) + 1
-bins2 = int(round((IR2axlim[1] - IR2axlim[0]) / cube)) + 1
-bins3 = int(round((IR3axlim[1] - IR3axlim[0]) / cube)) + 1
-bins4 = int(round((IR4axlim[1] - IR4axlim[0]) / cube)) + 1
-bins5 = int(round((MP1axlim[1] - MP1axlim[0]) / cube)) + 1
-print('Shape')
-print(binsa, bins1, bins2, bins3, bins4, bins5)
-#======================================================================
+# For now, only 6 out of 8 bands used
+all_axlim  = [Jaxlim, Ksaxlim, Haxlim, IR1axlim, IR2axlim, IR3axlim, IR4axlim, MP1axlim]
+bins_list  = [int(round((all_axlim[i][1] - all_axlim[i][0]) / cube)) + 1 for i in band_ID]
+axlim_list = [all_axlim[i] for i in band_ID]
+
+print('\ncubesize: ', cube)
+print('flux_ID: ', flux_ID)
+print('mag_ID: ', mag_ID)
+print('Qua/Qua_ID: ', qualabel, qua_ID)
+print('Shape: ', bins_list)
 
 # Check Directory
 if path.isdir('GPV_' + str(dim) + 'Dposvec_bin' + str(cube)):
@@ -68,14 +63,8 @@ if path.isdir('GPV_' + str(dim) + 'Dposvec_bin' + str(cube)):
 else:
     system('mkdir GPV_' + str(dim) + 'Dposvec_bin' + str(cube))
 
-#======================================================
 # Main Functions
-def drawProgressBar(percent, barLen = 50):
-    # percent float from 0 to 1.
-    sys.stdout.write("\r")
-    sys.stdout.write("[{:<{}}] {:.3f}%".format("=" * int(barLen * percent), barLen, (percent * 100)))
-    sys.stdout.flush()
-
+#======================================================
 @jit(nopython=True)
 def filter_bright_faint(pos_vec_array):
     '''
@@ -87,8 +76,7 @@ def filter_bright_faint(pos_vec_array):
     source = []
     for i in range(len(pos_vec_array)):
         pos_vec_array_sub = pos_vec_array[i]
-    #================================================
-    # Filiter out Bright/Faint Sources
+        # Filiter out Bright/Faint Sources
         if np.shape(np.where(pos_vec_array_sub == -9999)[0])[0] > 0:
             bright.append(pos_vec_array_sub)
         elif np.shape(np.where(pos_vec_array_sub == 9999)[0])[0] > 0:
@@ -98,69 +86,59 @@ def filter_bright_faint(pos_vec_array):
     return bright, faint, source
 
 @jit(nopython=True)
-def cascade_array(sort_position):
+def cascade_array_pos(sort_position):
     '''
     Use this to find out sources locate in same position and cascade them
     Note: No tuple() support in numba, No format() support in numba
     '''
-    #================================================
     # Input
     after_cascade_pos = []
     after_cascade_value = []
     start = 0
     end   = 0
     for i in range(len(sort_position)-1):
-        #================================================
-        # Indicator
-        #if i % 1000 == 0 and i>999:
-        #    print(float(i+1)/len(sort_position))
-        #================================================
         # Get reference and target
         tar, ref = sort_position[i], sort_position[i+1]
         end += 1
-        #================================================
         # Determine repeated or not
         if not np.all(np.equal(tar, ref)):
             after_cascade_pos.append(sort_position[start])
             after_cascade_value.append(end-start)
             start = end
-    #================================================
     # Include the last term
     after_cascade_pos.append(sort_position[start])
     after_cascade_value.append(len(sort_position)-start)
     return after_cascade_pos, after_cascade_value
 
+# Main Program
 #======================================================
-# Load Galaxy Catalog
-l_start = time.time()
-print("\nLoading input catalog ...")
-with open(str(argv[1]), 'r') as catalogs:
-    catalog = catalogs.readlines()
-l_end   = time.time()
-print("Loading catalog took {:.3f} secs\n".format(l_end-l_start))
+if __name__ == '__main__':
 
-#======================================================
-# Calculate Galaxy Position Vector
-c_start = time.time()
-pos_vec = []
-for i in range(len(catalog)):
-    #======================================================
-    # Percentage Indicator
-    drawProgressBar(float(i+1)/len(catalog))
-    #======================================================
-    # Unit transformation
-    line = catalog[i]
-    lines = line.split()
-    if datatype == 'flux':
-        mag_list = mJy_to_mag(lines, flux_ID=flux_ID, Qua=False, system="ukidss")
-    elif datatype == 'mag':
-        mag_list = mag_to_mag(lines, mag_ID=mag_ID, Qua=False, system="ukidss")
-    else:
-        print('Input type error')
-    magJ, magIR1, magIR2, magIR3, magIR4, magMP1 = mag_list
+    # Load Galaxy Catalog
+    l_start = time.time()
+    print("\nLoading input catalog ...")
+    with open(str(argv[1]), 'r') as catalogs:
+        catalog = catalogs.readlines()
+    l_end   = time.time()
+    print("Loading catalog took {:.3f} secs\n".format(l_end-l_start))
+
+    # Calculate Galaxy Position Vector
+    c_start = time.time()
+    pos_vec = []
+    for i in range(len(catalog)):
+        drawProgressBar(float(i+1)/len(catalog))
+        # Unit transformation
+        line = catalog[i]
+        lines = line.split()
+        if datatype == 'flux':
+            mag_list = mJy_to_mag(lines, flux_ID=flux_ID, Qua=qualabel, system=JHK_system)
+        elif datatype == 'mag':
+            mag_list = mag_to_mag(lines, mag_ID=mag_ID, Qua=qualabel, system=JHK_system)
+        else:
+            print('Input type error')
+    #magJ, magIR1, magIR2, magIR3, magIR4, magMP1 = mag_list
     SEQ = [sort_up_lack999(mag_list[i], axlim_list[i], cube) for i in range(len(axlim_list))]
     pos_vec.append(SEQ)
-
     #======================================================
     # Remove AGB sources (NOT considered in SEIP catalog)
     #AGB = 0
@@ -173,41 +151,37 @@ for i in range(len(catalog)):
     #    SEQ = [sort_up(mag_list[i], axlim_list[i], cube) for i in range(len(axlim_list))]
     #    pos_vec.append(SEQ)
     #======================================================
+    c_end   = time.time()
+    print("\n\nCalculate all sources position in n-dim space took {:.3f} secs\n".format(c_end-c_start))
 
-c_end   = time.time()
-print("\n\nCalculate all sources position in n-dim space took {:.3f} secs\n".format(c_end-c_start))
+    # Galaxy filter
+    f_start = time.time()
+    pos_vec_array = np.array(pos_vec)
+    np.save('test', pos_vec_array)
+    bright, faint, source = filter_bright_faint(pos_vec_array)
+    source_array = np.array(source)
+    f_end   = time.time()
+    print("Filter out bright and faint sources took {:.3f} secs\n".format(f_end-f_start))
 
-#======================================================
-# Galaxy filter
-f_start = time.time()
-pos_vec_array = np.array(pos_vec)
-np.save('test', pos_vec_array)
-bright, faint, source = filter_bright_faint(pos_vec_array)
-source_array = np.array(source)
-f_end   = time.time()
-print("Filter out bright and faint sources took {:.3f} secs\n".format(f_end-f_start))
+    # Sort Input Galaxy Position/Probability array
+    u_start = time.time()
+    position_t = np.transpose(source_array)
+    sort_ind = np.lexsort(tuple(position_t))
+    sort_position = np.array(source_array[sort_ind], dtype=int)
+    uni_pos, uni_num = cascade_array_pos(sort_position)
+    uni_pos_array = np.array(uni_pos)
+    uni_num_array = np.array(uni_num)
+    u_end   = time.time()
+    print("Sort sources took {:.3f} secs\n".format(u_end-u_start))
 
-#================================================
-# Sort Input Galaxy Position/Probability array
-u_start = time.time()
-position_t = np.transpose(source_array)
-sort_ind = np.lexsort(tuple(position_t))
-sort_position = np.array(source_array[sort_ind], dtype=int)
-uni_pos, uni_num = cascade_array(sort_position)
-uni_pos_array = np.array(uni_pos)
-uni_num_array = np.array(uni_num)
-u_end   = time.time()
-print("Sort sources took {:.3f} secs\n".format(u_end-u_start))
-
-#======================================================
-# Save Galaxy Position Vector, Bright, Faint
-s_start = time.time()
-chdir('GPV_' + str(dim) + 'Dposvec_bin' + str(cube))
-np.save('Gal_Position_vectors', uni_pos_array)
-np.save('Gal_Position_numbers', uni_num_array)
-np.save('Bright', bright)
-np.save('Faint',  faint)
-np.save('Shape',  np.array([binsa, bins1, bins2, bins3, bins4, bins5]))
-chdir('../')
-s_end   = time.time()
-print("Saving Galaxy Position took {:.3f} secs\n".format(s_end-s_start))
+    # Save Galaxy Position Vector, Bright, Faint
+    s_start = time.time()
+    chdir('GPV_' + str(dim) + 'Dposvec_bin' + str(cube))
+    np.save('Gal_Position_vectors', uni_pos_array)
+    np.save('Gal_Position_numbers', uni_num_array)
+    np.save('Bright', bright)
+    np.save('Faint', faint)
+    np.save('Shape', np.array(bins_list))
+    chdir('../')
+    s_end   = time.time()
+    print("Saving Galaxy Position took {:.3f} secs\n".format(s_end-s_start))
