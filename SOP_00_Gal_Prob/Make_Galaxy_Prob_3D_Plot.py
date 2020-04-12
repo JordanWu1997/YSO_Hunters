c23d062a140765f1c3ada26ce7892112f0ddf6ff
JordanWu <swingclouds@gmail.com>
2020-04-12 22:45:15 +0800

    add_load_all_6D_option_and_set_all_6D_default

--- SOP_00_Gal_Prob/Make_Galaxy_Prob_3D_Plot.py
+++ SOP_00_Gal_Prob/Make_Galaxy_Prob_3D_Plot.py
@@ -41,11 +41,12 @@ refD       = int(argv[5])       # Reference Beam Dimension
 deg_slice  = int(argv[6])
 incli      = int(argv[7])
 
+ddim       = 6                  # Specific dimensional data to plot
 shape_dir  = 'GPV_{:d}Dposvec_bin{:.1f}/'.format(dim, cube)
 smooth_dir = 'GPV_after_smooth_{:d}D_bin{:.1f}_sigma{:d}_bond{:d}_refD{:d}/'.format(dim, cube, sigma, bond, refD)
 output_dir = 'GPV_after_smooth_{:d}D_bin{:.1f}_sigma{:d}_bond{:d}_refD{:d}_GPtomo/'.format(dim, cube, sigma, bond, refD)
 all_shape  = np.load(shape_dir + 'Shape.npy')
-print('\n', all_shape)
+print('\n', all_shape, ddim)
 
 # Check storage directory
 if not path.isdir(output_dir):
@@ -68,7 +69,7 @@ def plot_3d_scatter(pos_array, num_array, shape, bd_ind, bd_name, sl_num, incli)
     '''
     Plot 2D plot along band 3
     '''
-    bd0, bd1, bd2 = pos_array[:, bd_ind[0]], pos_array[:, bd_ind[1]], pos_array[:, bd_ind[2]]
+    bd0, bd1, bd2 = pos_array[:, 0], pos_array[:, 1], pos_array[:, 2]
     bd0_g1, bd1_g1, bd2_g1 = bd0[num_array>1.], bd1[num_array>1.], bd2[num_array>1.]
     bd0_e1, bd1_e1, bd2_e1 = bd0[num_array==1.], bd1[num_array==1.], bd2[num_array==1.]
     for i, deg in enumerate(np.linspace(0, 360, sl_num, endpoint=False)):
@@ -76,8 +77,8 @@ def plot_3d_scatter(pos_array, num_array, shape, bd_ind, bd_name, sl_num, incli)
         fig = plt.figure(figsize=(12, 8))
         ax  = fig.add_subplot(111, projection='3d')
         #ax.scatter(bd0, bd1, bd2, s=0.5, alpha=0.8)
-        ax.scatter(bd0_g1, bd1_g1, bd2_g1, s=0.5, c='b', alpha=0.5, label='Scatter (GP>1)')
-        ax.scatter(bd0_e1, bd1_e1, bd2_e1, s=0.5, c='g', alpha=0.8, label='Scatter (GP=1)')
+        ax.scatter(bd0_g1, bd1_g1, bd2_g1, s=0.5, c='steelblue', alpha=0.3, label='Scatter (GP>1)')
+        ax.scatter(bd0_e1, bd1_e1, bd2_e1, s=0.5, c='g', alpha=1.0, label='Scatter (GP=1)')
         ax.set_title('{}{}{}_{}'.format(bd_name[0], bd_name[1], bd_name[2], bd_name[0]))
         ax.set_xlabel('{} ({:d})'.format(bd_name[0], shape[0]))
         ax.set_ylabel('{} ({:d})'.format(bd_name[1], shape[1]))
@@ -95,52 +96,63 @@ def plot_3d_scatter(pos_array, num_array, shape, bd_ind, bd_name, sl_num, incli)
 
 #=======================================================
 # Main Programs
-m_start = time.time()
-# Just for debugging test: band_ind_list = np.array([0, 1, 2]) band_ind_list = np.array([3, 4, 5])
-band_ind_list = np.arange(0, dim, 1)
-for comb in combinations(band_ind_list, 3):
+if __name__ == '__main__':
+
+    m_start = time.time()
+    # Just for debugging test: band_ind_list = np.array([0, 1, 2]) band_ind_list = np.array([3, 4, 5])
+    band_ind_list = np.arange(0, dim, 1)
+    for comb in combinations(band_ind_list, 3):
+
+        #=======================================================
+        # Generate band input
+        band_ind = ''
+        for band in comb:
+            band_ind += str(band)
+        bd_ind, shape, bd_name = [], [], []
+        for ind in band_ind:
+            bd_ind.append(int(ind))
+            shape.append(all_shape[int(ind)])
+            bd_name.append(band_name[int(ind)])
+        print('\n# band: ' + band_ind)
+
+        #=======================================================
+        # Load galaxy pos/num
+        l_start = time.time()
+        if ddim == dim:
+            gal_pos = np.load(smooth_dir + 'after_smooth_lack_{:d}_{}_all_cas_pos.npy'.format(0, ''.join([str(i) for i in range(ddim)])))
+            gal_num = np.load(smooth_dir + 'after_smooth_lack_{:d}_{}_all_cas_num.npy'.format(0, ''.join([str(i) for i in range(ddim)])))
+            gal_pos_1, gal_pos_2, gal_pos_3 = np.array([gal_pos[:, bd_ind[0]]]), \
+                                              np.array([gal_pos[:, bd_ind[1]]]), \
+                                              np.array([gal_pos[:, bd_ind[2]]])
+            gal_con = np.concatenate((gal_pos_1, gal_pos_2, gal_pos_3), axis=0)
+            gal_pos = np.transpose(gal_con)
+        else:
+            gal_pos = np.load(smooth_dir + 'after_smooth_lack_{:d}_{}_all_cas_pos.npy'.format(dim-len(band_ind), band_ind))
+            gal_num = np.load(smooth_dir + 'after_smooth_lack_{:d}_{}_all_cas_num.npy'.format(dim-len(band_ind), band_ind))
+        out_pos, out_num = sort_inp_gal(gal_pos, gal_num)
+        pos_array, num_array = np.array(out_pos), np.array(out_num)
+        l_end   = time.time()
+        #print('Loading took {:.3f} secs'.format(l_end-l_start))
+
+        #=======================================================
+        # Start plotting
+        p_start = time.time()
+        chdir(output_dir)
+        tomo_dir = 'tomo_{}/'.format(band_ind)
+        if not path.isdir(tomo_dir):
+            system('mkdir {}'.format(tomo_dir))
+        chdir(tomo_dir)
+        all_dir  = 'all_{}_n{:d}_i{:d}/'.format(band_ind, deg_slice, incli)
+        if not path.isdir(all_dir):
+            system('mkdir {}'.format(all_dir))
+        chdir(all_dir)
+        plot_3d_scatter(pos_array, num_array, shape, bd_ind, bd_name, deg_slice, incli)
+        chdir('../')
+        system('convert -delay 100 -loop 0 {}*.png {}_all_{}_n{:d}_i{:d}.gif'.format(all_dir, band_ind, bd_ind[0], deg_slice, incli))
+        chdir('../../')
+        p_end   = time.time()
+        print('\nPlotting took {:.3f} secs'.format(p_end-p_start))
 
     #=======================================================
-    # Generate band input
-    band_ind = ''
-    for band in comb:
-        band_ind += str(band)
-    bd_ind, shape, bd_name = [], [], []
-    for ind in band_ind:
-        bd_ind.append(int(ind))
-        shape.append(all_shape[int(ind)])
-        bd_name.append(band_name[int(ind)])
-    print('\n# band: ' + band_ind)
-
-    #=======================================================
-    # Load galaxy pos/num
-    l_start = time.time()
-    gal_pos = np.load(smooth_dir + 'after_smooth_lack_{:d}_{}_all_cas_pos.npy'.format(dim-len(band_ind), band_ind))
-    gal_num = np.load(smooth_dir + 'after_smooth_lack_{:d}_{}_all_cas_num.npy'.format(dim-len(band_ind), band_ind))
-    out_pos, out_num = sort_inp_gal(gal_pos, gal_num)
-    pos_array, num_array = np.array(out_pos), np.array(out_num)
-    l_end   = time.time()
-    #print('Loading took {:.3f} secs'.format(l_end-l_start))
-
-    #=======================================================
-    # Start plotting
-    p_start = time.time()
-    chdir(output_dir)
-    tomo_dir = 'tomo_{}/'.format(band_ind)
-    if not path.isdir(tomo_dir):
-        system('mkdir {}'.format(tomo_dir))
-    chdir(tomo_dir)
-    all_dir  = 'all_{}_n{:d}_i{:d}/'.format(band_ind, deg_slice, incli)
-    if not path.isdir(all_dir):
-        system('mkdir {}'.format(all_dir))
-    chdir(all_dir)
-    plot_3d_scatter(pos_array, num_array, shape, bd_ind, bd_name, deg_slice, incli)
-    chdir('../')
-    system('convert -delay 100 -loop 0 {}*.png {}_all_{}_n{:d}_i{:d}.gif'.format(all_dir, band_ind, bd_ind[0], deg_slice, incli))
-    chdir('../../')
-    p_end   = time.time()
-    print('\nPlotting took {:.3f} secs'.format(p_end-p_start))
-
-#=======================================================
-m_end   = time.time()
-print('\nWhole Process took {:.3f} secs\n'.format(m_end-m_start))
+    m_end   = time.time()
+    print('\nWhole Process took {:.3f} secs\n'.format(m_end-m_start))
