97e96d7d89214d7a0e65ad16828b32345e32b414
JordanWu <swingclouds@gmail.com>
2020-05-22 18:49:06 +0800

    add_fc_and_sc_plot_style_and_remove_all_weighted_pca

--- SOP_00_Gal_Prob/Make_Galaxy_Prob_Plot_Execution_All.py
+++ SOP_00_Gal_Prob/Make_Galaxy_Prob_Plot_Execution_All.py
@@ -37,7 +37,6 @@ GP3D_Plot_PCA = 'Make_Galaxy_Prob_3D_Plot_With_PCA.py'
 #=======================================================
 # Main Program
 if __name__ == '__main__':
-
     t_start = time.time()
 
     # Make_Galaxy_Prob_2D_Plot.py
@@ -48,17 +47,15 @@ if __name__ == '__main__':
     system('{} {:d} {:.1f} {:d} {:d} {:d} {:d} {:d}'.format(\
             GP3D_Plot, dim, cube, sigma, bond, refD, deg_slice, incli))
 
-    # Make_Galaxy_Prob_PCA_Cut_Plot.py (Weighted, Non-weighted)
-    system('{} {:d} {:.1f} {:d} {:d} {:d} {} {} {} {}'.format(\
-            GPPCA_Cut, dim, cube, sigma, bond, refD, '0', ''.join([str(i) for i in range(dim)]), 'True'))
-    system('{} {:d} {:.1f} {:d} {:d} {:d} {} {} {} {}'.format(\
-            GPPCA_Cut, dim, cube, sigma, bond, refD, '0', ''.join([str(i) for i in range(dim)]), 'False'))
+    # Make_Galaxy_Prob_PCA_Cut_Plot.py (Non-weighted, full cut and simple cut)
+    system('{} {:d} {:.1f} {:d} {:d} {:d} {} {} {} {} {}'.format(\
+            GPPCA_Cut, dim, cube, sigma, bond, refD, '0', ''.join([str(i) for i in range(dim)]), 'False', 'sc'))
+    system('{} {:d} {:.1f} {:d} {:d} {:d} {} {} {} {} {}'.format(\
+            GPPCA_Cut, dim, cube, sigma, bond, refD, '0', ''.join([str(i) for i in range(dim)]), 'False', 'fc'))
 
     # Make_Galaxy_Prob_3D_Plot_With_PCA (Weigthed, Non-weighted)
     system('{} {:d} {:.1f} {:d} {:d} {:d} {:d} {:d} {} {}'.format(\
             GP3D_Plot_PCA, dim, cube, sigma, bond, refD, deg_slice, incli, 'True', 'True'))
-    system('{} {:d} {:.1f} {:d} {:d} {:d} {:d} {:d} {} {}'.format(\
-            GP3D_Plot_PCA, dim, cube, sigma, bond, refD, deg_slice, incli, 'False', 'True'))
 
     t_end   = time.time()
     print('{} took {:.3f} secs'.format(argv[0], t_end-t_start))
