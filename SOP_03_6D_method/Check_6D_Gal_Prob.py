98935e49a233d490a36bbad9cce25517ff1f91a6
JordanWu <swingclouds@gmail.com>
2020-05-24 23:47:35 +0800

    make_it_simpler_and_assign_ID

--- SOP_03_6D_method/Check_6D_Gal_Prob.py
+++ SOP_03_6D_method/Check_6D_Gal_Prob.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-'''-------------------------------------------------------------------------
+'''
+-------------------------------------------------------------------------
 This program is to check 6d galaxy probability and sort the candidates
 
 Input: catalog after galaxy probability (P)  calculation
@@ -7,74 +8,101 @@ Input: catalog after galaxy probability (P)  calculation
 Output: (1)YSO: YSO candidates for sure
         (2)Galaxy: Galaxy candidates fo sure
         (3)GP_IC: GP with problems to image check
-----------------------------------------------------------------------------
-latest update: 2018/12/11'''
 
+Major_Update at 2020: Make it more general and simpler
+-------------------------------------------------------------------------
+latest update: 20200524 Jordan Wu'''
+
+from __future__ import print_function
+from sys import argv, exit
+from time import time
 import os
-from sys import argv
-from sys import exit
 
+# Check Inputs
+#=========================================================================
 if len(argv) != 3:
-    exit('Wrong Usage!\nExample: ipython [program] [catalog] [MC cloud]')
+    exit('\n\tWrong Usage!\
+          \n\tExample: [program] [catalog] [MC cloud]\n')
 
+# Input Variables
+#=========================================================================
 cloud = str(argv[2])
-catalog = open(argv[1], 'r')
-cat = catalog.readlines()
+RA_ID  = 1
+DEC_ID = 3
+MP1_Qua_ID = 185
+GP_OBJ_ID, GP_ID = 241, 242
+GPP_OBJ_ID, GPP_ID = 243, 244
 
-#-----------------------------------------------------------------------------------------------------------
-
-YSO = []; Galaxy = []; GP_IC = []
-for i in range(len(cat)):
-    row_prob = cat[i].split()
-
-    if row_prob[242] != 'no_count':
-        GP = float(row_prob[242])
+# Functions
+#=========================================================================
+def classify_by_GP(row_list):
+    '''
+    This is to classify input object by GP and GPP and return object label
+    '''
+    if row_list[GP_ID] != 'no_count':
+        GP = float(row_list[GP_ID])
         if GP <= 1:
-            if row_prob[244] != 'no_count':
-                GPP = float(row_prob[244])
+            if row_list[GPP_ID] != 'no_count':
+                GPP = float(row_list[GPP_ID])
                 if GPP > 1:
-                    GP_IC.append(cat[i])
+                    label = 'GP_IC'
                 elif GPP <= 1:
-                    YSO.append(cat[i])
+                    label = 'YSO'
             else:
-                GP_IC.append(cat[i])
-
+                label = 'GP_IC'
         elif GP > 1:
-            if row_prob[244] != 'no_count':
-                GPP = float(row_prob[244])
+            if row_list[GPP_ID] != 'no_count':
+                GPP = float(row_list[GPP_ID])
                 if GP <= 1:
-                    GP_IC.append(cat[i])
+                    label = 'GP_IC'
                 elif GP > 1:
-                    Galaxy.append(cat[i])
+                    label = 'Galaxy'
             else:
-                Galaxy.append(cat[i])
+                label = 'Galaxy'
     else:
-        Galaxy.append(cat[i])
+        label = 'Galaxy'
+    return label
 
-#-----------------------------------------------------------------------------------------------------------
+# Main Programs
+#=========================================================================
+if __name__ == '__main__':
+    s_start = time.time()
 
-out_ca = open(cloud + '_6D_YSO.tbl',"w")
-for i in YSO:
-    out_ca.write(str(i))
-out_ca.close()
+    # Load catalogs
+    with open(argv[1], 'r') as cat:
+        catalog = cat.readlines()
 
-out_ca = open(cloud + '_6D_Galaxy.tbl',"w")
-for i in Galaxy:
-    out_ca.write(str(i))
-out_ca.close()
+    # Classify input objects
+    YSO, Galaxy, GP_IC = [], [], []
+    for i in range(len(catalog)):
+        row_list = catalog[i].split()
+        label = classify_by_GP(row_list)
+        if label == 'YSO':
+            YSO.append(row_list)
+        elif label == 'Galaxy':
+            Galaxy.append(row_list)
+        elif label == 'GP_IC':
+            GP_IC.append(row_list)
 
-out_cat = open(cloud + '_6D_GP_to_image_check.tbl',"w")
-for i in GP_IC:
-    out_cat.write(str(i))
-out_cat.close()
+    # Write different object catalogs
+    with open(cloud + '_6D_YSO.tbl',"w") as out_cat:
+        for i in YSO:
+            out_cat.write(str(i))
+    with open(cloud + '_6D_Galaxy.tbl',"w") as out_cat:
+        for i in Galaxy:
+            out_cat.write(str(i))
+    with open(cloud + '_6D_GP_to_image_check.tbl',"w") as out_cat:
+        for i in GP_IC:
+            out_cat.write(str(i))
 
-#-----------------------------------------------------------------------------------------------------------
-print('The Saturate Candiates in YSO candidates: ')
-os.system('echo | awk \'$185 == \"S\" {print $1, $3} \' '+ cloud + '_6D_YSO.tbl')
-print('The confident YSO candidates: ')
-os.system('wc ' + cloud + '_6D_YSO.tbl')
-print('The confident Galaxy candidates: ')
-os.system('wc ' + cloud + '_6D_Galaxy.tbl')
-print('The candidate to image check ')
-os.system('wc ' + cloud + '_6D_GP_to_image_check.tbl')
-#-----------------------------------------------------------------------------------------------------------
+    # Print out catalog line numbers
+    print('The Saturate Candiates in YSO candidates: ')
+    os.system('echo | awk \'${:d} == \"S\" {print ${:d}, ${:d}} \' {}_6D_YSO.tbl'.format(MP1_Qua_ID, RA_ID, DEC_ID, cloud))
+    print('The confident YSO candidates: ')
+    os.system('wc ' + cloud + '_6D_YSO.tbl')
+    print('The confident Galaxy candidates: ')
+    os.system('wc ' + cloud + '_6D_Galaxy.tbl')
+    print('The candidate to image check: ')
+    os.system('wc ' + cloud + '_6D_GP_to_image_check.tbl')
+    s_end   = time.time()
+    print('Whole {} took {:.3f} secs'.format(argv[0], s_end - s_start))
