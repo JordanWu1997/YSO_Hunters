716085438a3f6b39349e58f785d308b1d9953e2a
JordanWu <swingclouds@gmail.com>
2020-06-04 12:16:58 +0800

    update_description_and_default_values

--- setup.py
+++ setup.py
@@ -1,19 +1,41 @@
 #!/usr/bin/python
+'''
+----------------------------------------------------------------------
+Abstract:
+    This is to change python working path for all YSO Hunters programs
+
+Example: [program] [python path]
+
+Input Variables:
+    [python path]: python working path
+----------------------------------------------------------------------
+Latest updated: 2020/06/04 Jordan Wu'''
 
 from glob import glob
 from os import system
 from sys import argv, exit
 
-print('\nFiles to change head line')
-exec_file_list = []
-for dirs in glob('./*'):
-    for files in glob('{}/*'.format(dirs)):
-        if '.py' in files:
-            exec_file_list.append(files)
-            print(files)
+if __name__ == '__main__':
 
-headline = str(argv[1])
-for files in exec_file_list:
-    system("sed -i \'1s/.*/{}/\' {}".format(headline, files))
-print('\nDefault HL: #!/usr/bin/python\
-       \nNew HL    : #!{}'.format(headline))
+    # Check inputs
+    if len(argv) != 2:
+        exit('\n\tExample: [program] [python path]\
+              \n\t[python path]: python working path\n')
+
+    # Get file list
+    print('\nFiles to change head line')
+    exec_file_list = []
+    for dirs in glob('./*'):
+        for files in glob('{}/*'.format(dirs)):
+            if '.py' in files:
+                exec_file_list.append(files)
+                print(files)
+
+    # Change headline
+    headline = '#!{}'.format(str(argv[1]))
+    if headline == 'default':
+        headline = '#!/usr/bin/python'
+    for files in exec_file_list:
+        system("sed -i \'1s/.*/{}/\' {}".format(headline, files))
+    print('\nDefault HL: #!/usr/bin/python\
+           \nNew HL    : {}'.format(headline))
