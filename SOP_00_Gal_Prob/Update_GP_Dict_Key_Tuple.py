273c3820d8ba5c560e8d38a5477d2fc69b6d290d
JordanWu <swingclouds@gmail.com>
2020-07-14 15:16:41 +0800

    update

---
+++ SOP_00_Gal_Prob/Update_GP_Dict_Key_Tuple.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+'''
+------------------------------------------------------------------------------------------------------------
+Example: [program] [dim] [cube size] [sigma] [bond] [ref-D]
+    Input Variables:
+    [dim]:       dimension for smooth (for now only "6")\
+    [cube size]: length of multi-d cube in magnitude unit\
+    [sigma]:     standard deviation for gaussian dist. in magnitude\
+    [bond]:      boundary radius of gaussian beam unit in cell\
+    [ref-D]:     reference dimension which to modulus other dimension to\n')
+
+Caution: This program must be run under PYTHON 2
+         Otherwise, numpy dictionary array loading will go wrong ...
+------------------------------------------------------------------------------------------------------------
+Latest Updated: 2020.07.14 Jordan Wu'''
+
+# Import Modules
+#==========================================================
+from __future__ import print_function
+from sys import argv, exit
+from All_Variables import *
+from Useful_Functions import *
+from Hsieh_Functions import *
+import numpy as np
+import time
+import glob
+
+# Functions
+#==========================================================
+
+# Main Program
+#==========================================================
+if __name__ == '__main__':
+    s_start = time.time()
+
+    # Check inputs
+    if len(argv) != 6:
+        exit('\n\tError: Wrong Arguments\
+        \n\tExample: [program] [dim] [cube size] [sigma] [bond] [ref-D]\
+        \n\t[dim]: dimension for smooth (for now only "6")\
+        \n\t[cube size]: length of multi-d cube in magnitude unit\
+        \n\t[sigma]: standard deviation for gaussian dist. in magnitude\
+        \n\t[bond]: boundary radius of gaussian beam unit in cell\
+        \n\t[ref-D]: reference dimension which to modulus other dimension to\n')
+
+    # Input variables
+    dim         = int(argv[1])       # Dimension of position vector
+    cube        = float(argv[2])     # Beamsize for each cube
+    sigma       = int(argv[3])       # STD for Gaussian Smooth
+    bond        = int(argv[4])       # Bond for Gaussian Smooth
+    refD        = int(argv[5])       # Reference Beam Dimension
+    out_prefix  = 'GPV_after_smooth_{:d}D_bin{:.1f}_sigma{:d}_bond{:d}_refD{:d}'.format(dim, cube, sigma, bond, refD)
+    out_dir     = '{}/'.format(out_prefix)
+
+    # Update dictionary for different lack band number
+    for lack in range(dim-3+1):
+        print('\nCombining Lack {:d} sources pos/num...'.format(lack))
+
+        # Generate different lack list
+        lack_pos_ls = glob.glob('{}/after_smooth_lack_{:d}_*_all_cas_pos.npy'.format(out_prefix, lack))
+        lack_num_ls = glob.glob('{}/after_smooth_lack_{:d}_*_all_cas_num.npy'.format(out_prefix, lack))
+        lack_pos_arr_ls = [np.load(lack_pos) for lack_pos in lack_pos_ls]
+        lack_num_arr_ls = [np.load(lack_num) for lack_num in lack_num_ls]
+        print('\n'.join(lack_pos_ls))
+        print('\n'.join(lack_num_ls))
+
+        # Combined different lack list into one
+        comb_num, tot_num = 0, sum([len(lack_num_arr) for lack_num_arr in lack_num_arr_ls])
+        lack_pos_list, lack_num_list = [], []
+        for i in range(len(lack_num_arr_ls)):
+            lack_pos_arr = lack_pos_arr_ls[i]
+            lack_num_arr = lack_num_arr_ls[i]
+            for j in range(len(lack_num_arr)):
+                lack_pos = lack_pos_arr[j]
+                lack_num = lack_num_arr[j]
+                lack_pos_list.append(lack_pos)
+                lack_num_list.append(lack_num)
+                comb_num += 1
+                drawProgressBar(float(comb_num) / tot_num)
+
+        # Save output dictionary (key in tuple)
+        print('\n\nSaving Lack {:d} sources dictionary'.format(lack))
+        out_dict = dict()
+        for i in range(len(lack_num_list)):
+            out = {tuple(lack_pos_list[i]): float(lack_num_list[i])}
+            out_dict.update(out)
+
+        print(out_dict)
+        np.save('{}all_detect_grid_Full_{:d}d.npy'.format(out_dir, dim-lack), out_dict)
+        print('{}all_detect_grid_Full_{:d}d.npy'.format(out_dir, dim-lack))
+
+    # Print out result ...
+    s_end   = time.time()
+    print('\nUpdate Galaxy Probability Dictionary For all Lack Bands took {:.3f} secs\n'.format(s_end-s_start))
---
+++ SOP_03_6D_method/Backup/Calculate_GP_WI_6D_Dict_Key_Tuple.py
@@ -0,0 +1,364 @@
+#!/usr/bin/python
+'''----------------------------------------------------------------
+This program is for calculating 6-d galaxy probability (P)
+
+Input : catalog with
+        (1)5-d galaxy probablity
+        (2)galaxy probability P
+
+Output: catalog with
+        (1)6-d galaxy probability
+        (2)6-d galaxy probability P
+*Note:
+    (1)6 bands are J, IR1, IR2, IR3, IR4, MP1
+    (2)Some function are imported from Hsieh_Functions.py
+    #================================================================
+    (3)Gal_Prob=0 is now assigned to Gal_Prob =1e-9 (Originally, 1e-3)
+    #================================================================
+
+**New add function: computing with different binsizes
+-------------------------------------------------------------------
+latest update : 2019/02/26 Jordan Wu'''
+
+#======================================================================================
+# Setup initial environment; Check input arguments
+#======================================================================================
+import os
+import time
+import numpy as np
+from os import system
+from sys import argv, exit
+from All_Variables import *
+from Hsieh_Functions import *
+import SOP_Program_Path as spp
+
+if len(argv) == 4 or len(argv) == 6:
+    print('Start 6D Galaxy Prob calculating ...')
+else:
+    exit('Error: Wrong Usage!\n \
+          Exmaple: python [program] [catalog] [cloud\'s name] [data_type]\n \
+          data_type: flux or mag (default=flux)\n')
+
+#======================================================================================
+# Start loading Galaxy probabilty dictionary
+#======================================================================================
+if str(argv[-1]) == 'argv':
+    Binsize = str(argv[-2])
+else:
+    Binsize = str(input('binsize = '))
+
+#if str(argv[4]) == 'old':
+#    #path = '/home/ken/new_mg/GPV_SOP_Program/result' + Binsize + '/'
+#    path = spp.Selfmade_6D_GP_Dict_path + '/result' + Binsize + '/'
+#elif str(argv[4]) == 'new':
+#    path = spp.Selfmade_6D_GP_Dict_path + '/result_condition_' + Binsize + '/'
+#elif str(argv[4]) == 'latest':
+#    path = spp.Selfmade_6D_GP_Dict_path ###################### TO BE CONTINUED ...
+#else:
+#    exit('Wrong model selection ...')
+#print('Array path: ' + path)
+
+tStart = time.time()
+path = spp.Selfmade_6D_GP_Dict_path
+print('Array: ' + path)
+print('Loading arrays ...')
+
+# New type galaxy position in dictionary
+Fu_Dict = np.load(path + 'all_detect_grid_Full_6d.npy').item()
+L1_Dict = np.load(path + 'all_detect_grid_Full_5d.npy').item()
+L2_Dict = np.load(path + 'all_detect_grid_Full_4d.npy').item()
+L3_Dict = np.load(path + 'all_detect_grid_Full_3d.npy').item()
+
+tEnd = time.time()
+print("Loading arrays took %f sec" % (tEnd - tStart))
+time.sleep(2)
+
+#======================================================================================
+# Set up parameters
+#======================================================================================
+print('Loading catalog ...')
+table = open(str(argv[1]), 'r')
+catalog = table.readlines()
+table.close()
+
+Cloud = str(argv[2])
+data_type = str(argv[3])
+
+#parameter
+cube = float(Binsize)
+band_name = ['J','IR1','IR2','IR3','IR4','MP1']
+
+# Use Limit Stored in Hsieh_Functions
+Jaxlim   = Hsieh_Jaxlim
+Ksaxlim  = Hsieh_Ksaxlim
+IR1axlim = Hsieh_IR1axlim
+IR2axlim = Hsieh_IR2axlim
+IR3axlim = Hsieh_IR3axlim
+IR4axlim = Hsieh_IR4axlim
+MP1axlim = Hsieh_MP1axlim
+
+#Jaxlim   = Jaxlim
+#Ksaxlim  = Ksaxlim
+#IR1axlim = IR1axlim
+#IR2axlim = IR2axlim
+#IR3axlim = IR3axlim
+#IR4axlim = IR4axlim
+#MP1axlim = MP1axlim
+
+#======================================================================================
+# Start calculating 6-D Galaxy probabilty
+#======================================================================================
+tStart = time.time()
+print('Start Calculating ...')
+out = []
+for i in range(len(catalog)):
+
+    # Percentage Indicator
+    if i % 100 == 1:
+        print('%.6f' % (float(i)/float(len(catalog))*100) + '%')
+    # Set up initial condition
+    line = catalog[i].split()
+
+    #===============================================
+    if data_type == 'flux':
+        mag_list = magnitudelist(line)
+
+    # Command below is for UKIDSS-SWIRE type catalog
+    elif data_type == 'mag':
+        mag_list = mag_magnitudelist(line)
+
+    # Default: flux type
+    else:
+        mag_list = magnitudelist(line)
+    #===============================================
+
+    magJ = mag_list[0]; magIR1 = mag_list[1]; magIR2 = mag_list[2]; magIR3 = mag_list[3]; magIR4 = mag_list[4]; magMP1 = mag_list[5]
+    PSF_list = [line[39], line[102], line[123], line[144], line[165], line[186]]
+    SEQ = [seq(magJ,Jaxlim,cube), seq(magIR1,IR1axlim,cube), seq(magIR2,IR2axlim,cube), seq(magIR3,IR3axlim,cube), seq(magIR4,IR4axlim,cube), seq(magMP1,MP1axlim,cube)]
+    num = 6 - SEQ.count('Lack')
+    ob_type = str(num) + "bands_"
+    count = 'no_count'
+    KEY = 'NO_KEY'
+
+    # Remove AGB
+    de="unknown"
+    if magIR2 != 'no' and magIR3 != 'no' and magMP1 != 'no':
+        X23 = magIR2 - magIR3; Y35 = magIR3 - magMP1
+        if index(X23,Y35,[0,0,2,5],[-1,0,2,2]) < 0:
+            de = "AGB"
+            ob_type += "AGB_"
+            count = "no_count"
+
+    # Sort with detected band num
+    if num >= 3 and de != "AGB":
+
+        # Set up parameters for searching lack bands
+        KEY = (seq(magJ,Jaxlim,cube), seq(magIR1,IR1axlim,cube), seq(magIR2,IR2axlim,cube), seq(magIR3, IR3axlim,cube), seq(magIR4,IR4axlim,cube), seq(magMP1,MP1axlim,cube))
+        key_array = np.array(KEY)
+        index_array = np.argwhere(key_array=='Lack')
+
+        if SEQ.count('Faint') > 0:
+            count = 99999
+            ob_type += 'Faint'
+
+        elif SEQ.count('Bright') > 0:
+            count = 1e-4
+            ob_type += 'Bright'
+
+        elif num == 6:
+            try:
+                count = Fu_Dict[KEY]
+                ob_type += "Lack_no_"
+            except KeyError:
+                count = 1e-5
+                ob_type += "Lack_no_"
+                ob_type += '_6D_NOGALAXY_'
+
+        elif num == 5:
+            try:
+                count = L1_Dict[KEY]
+                ob_type += "Lack_" + band_name[int(index_array[0])]
+            except KeyError:
+                count = 1e-5
+                ob_type += "Lack_" + band_name[int(index_array[0])]
+                ob_type += '_5D_NOGALAXY_'
+
+        elif num == 4:
+            try:
+                count = L2_Dict[KEY]
+                ob_type += "Lack_" + band_name[int(index_array[0])] + band_name[int(index_array[1])]
+            except KeyError:
+                count = 1e-5
+                ob_type += "Lack_" + band_name[int(index_array[0])] + band_name[int(index_array[1])]
+                ob_type += '_4D_NOGALAXY_'
+
+        elif num == 3:
+            try:
+                count = L3_Dict[KEY]
+                ob_type += "Lack_" + band_name[int(index_array[0])] + band_name[int(index_array[1])] + band_name[int(index_array[2])]
+            except KeyError:
+                count = 1e-5
+                ob_type += "Lack_" + band_name[int(index_array[0])] + band_name[int(index_array[1])] + band_name[int(index_array[2])]
+                ob_type += '_3D_NOGALAXY_'
+
+        # Avoid log(0)
+        if count == 0.0:
+            count = 1e-9
+
+    if line[184] == "S":
+        count = 1e-4
+
+    ob_type += "bandfill=" + str(PSF_list.count("-2"))
+
+    #Just to create some empty columns
+    if len(line) < 246:
+        while len(line) <= 246:
+            line.append('z')
+    line[241] = ob_type
+    line[242] = str(count)
+    line[245] = str(KEY)
+
+    out.append("\t".join(line))
+
+tEnd = time.time()
+print("Calculating 6D_Gal_Prob took %f sec" % (tEnd - tStart))
+
+#======================================================================================
+# END of calculating 6D GALAXY PROB; Save the result
+#======================================================================================
+if os.path.isfile(Cloud + "_6D_GP_out_catalog"):
+    system('rm ./' + Cloud + "_6D_GP_out_catalog")
+out = "\n".join(out) + '\n'
+out_ca = open(Cloud + "_6D_GP_out_catalog","w")
+out_ca.write(out)
+out_ca.close()
+
+#======================================================================================
+# START of calculating 6D GALAXY PROB P
+#======================================================================================
+print('Loading catalog ...')
+table = open(Cloud + "_6D_GP_out_catalog","r")
+catalog = table.readlines()
+table.close()
+tStart = time.time()
+print('Start Calculating ...')
+
+out = []
+for i in range(len(catalog)):
+
+    # Percentage Indicator
+    if i % 100 == 1:
+        print('%.6f' % (float(i)/float(len(catalog))*100) + '%')
+
+    # Set up initial condition
+    line = catalog[i].split()
+
+    #===============================================
+    if data_type == 'flux':
+        mag_list = PSF_magnitudelist(line)
+
+    # Command below is for UKIDSS-SWIRE type catalog
+    elif data_type == 'mag':
+        mag_list = mag_PSF_magnitudelist(line)
+
+    # Default: flux type
+    else:
+        mag_list = PSF_magnitudelist(line)
+    #===============================================
+
+    magJ = mag_list[0]; magIR1 = mag_list[1]; magIR2 = mag_list[2]; magIR3 = mag_list[3]; magIR4 = mag_list[4]; magMP1 = mag_list[5]
+    PSF_list = [line[39], line[102], line[123], line[144], line[165], line[186]]
+    SEQ = [seq(magJ,Jaxlim,cube), seq(magIR1,IR1axlim,cube), seq(magIR2,IR2axlim,cube), seq(magIR3,IR3axlim,cube), seq(magIR4,IR4axlim,cube), seq(magMP1,MP1axlim,cube)]
+    num = 6 - SEQ.count('Lack')
+    ob_type = str(num) + "bands_"
+    count = 'no_count'
+    KEY = '_'
+
+    # Remove AGB
+    de="unknown"
+    if magIR2 != 'no' and magIR3 != 'no' and magMP1 != 'no':
+        X23 = magIR2 - magIR3; Y35 = magIR3 - magMP1
+        if index(X23,Y35,[0,0,2,5],[-1,0,2,2]) < 0:
+            de = "AGB"
+            ob_type += "AGB_"
+            count = "no_count"
+
+    # Sort with detected band num
+    if num >= 3 and de != "AGB":
+
+        # Set up parameters for searching lack bands
+        KEY = (seq(magJ,Jaxlim,cube), seq(magIR1,IR1axlim,cube),seq(magIR2,IR2axlim,cube), seq(magIR3,IR3axlim,cube), seq(magIR4,IR4axlim,cube), seq(magMP1,MP1axlim,cube))
+        key_array = np.array(KEY)
+        index_array = np.argwhere(key_array=='Lack')
+
+        if SEQ.count('Faint') > 0:
+            count = 99999
+            ob_type += 'Faint'
+
+        elif SEQ.count('Bright') > 0:
+            count = 1e-4
+            ob_type += 'Bright'
+
+        elif num == 6:
+            try:
+                count = Fu_Dict[KEY]
+                ob_type += "Lack_no_"
+            except KeyError:
+                count = 1e-5
+                ob_type += "Lack_no_"
+                ob_type += '_6D_NOGALAXY_'
+
+        elif num == 5:
+            try:
+                count = L1_Dict[KEY]
+                ob_type += "Lack_" + band_name[int(index_array[0])]
+            except KeyError:
+                count = 1e-5
+                ob_type += "Lack_" + band_name[int(index_array[0])]
+                ob_type += '_5D_NOGALAXY_'
+
+        elif num == 4:
+            try:
+                count = L2_Dict[KEY]
+                ob_type += "Lack_" + band_name[int(index_array[0])] + band_name[int(index_array[1])]
+            except KeyError:
+                count = 1e-5
+                ob_type += "Lack_" + band_name[int(index_array[0])] + band_name[int(index_array[1])]
+                ob_type += '_4D_NOGALAXY_'
+
+        elif num == 3:
+            try:
+                count = L3_Dict[KEY]
+                ob_type += "Lack_" + band_name[int(index_array[0])] + band_name[int(index_array[1])] + band_name[int(index_array[2])]
+            except KeyError:
+                count = 1e-5
+                ob_type += "Lack_" + band_name[int(index_array[0])] + band_name[int(index_array[1])] + band_name[int(index_array[2])]
+                ob_type += '_3D_NOGALAXY_'
+
+        # Avoid log(0)
+        if count == 0.0:
+            count = 1e-9
+
+    if line[184] == "S":
+        count = 1e-4
+
+    ob_type += "bandfill=" + str(PSF_list.count("-2"))
+    line[243] = ob_type
+    line[244] = str(count)
+    out.append("\t".join(line))
+
+tEnd = time.time()
+print("Calculating 6D Gal_Prob_P took %f sec" % (tEnd - tStart))
+
+#======================================================================================
+# END of calculating 6D GALAXY PROB P; Clean old results and Save the new results
+#======================================================================================
+if os.path.isfile(Cloud + "_6D_GP_all_out_catalog.tbl"):
+    system('rm ' + Cloud + "_6D_GP_all_out_catalog.tbl")
+out = "\n".join(out) + '\n'
+out_ca = open(Cloud + "_6D_GP_all_out_catalog.tbl","w")
+out_ca.write(out)
+out_ca.close()
+
+print('6D Gal Prob calculation ends ...')
+system('rm ' + Cloud + "_6D_GP_out_catalog")
