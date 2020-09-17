f89f6eb6b8c84d4d1c9ea9889d20d568507da672
JordanWu <swingclouds@gmail.com>
2020-09-18 02:37:22 +0800

    fix_5D_issue

--- SOP_00_Gal_Prob/Find_Galaxy_Prob_6D_Boundary_Along_Band_Parallel.py
+++ SOP_00_Gal_Prob/Find_Galaxy_Prob_6D_Boundary_Along_Band_Parallel.py
@@ -21,32 +21,46 @@ Latest Updated: 2020.08.26 Jordan Wu'''
 from __future__ import print_function
 from sys import argv, exit
 from os import chdir
-from All_Variables import *
-from Useful_Functions import *
-from Hsieh_Functions import *
+from argparse import ArgumentParser
 from joblib import Parallel, delayed
 import numpy as np
 import time
+from All_Variables import *
+from Useful_Functions import *
+from Hsieh_Functions import *
 
 # Functions
 #==========================================================
-def generate_6D_origins_on_plane(band_inp, sc_fixed_bd, sc_lower_bd, sc_upper_bd):
+def generate_origins_on_plane(sc_fixed_bd, sc_lower_bd, sc_upper_bd):
     '''
     This is used to generate origins on specific plane (fixed one band)
     This is especially for 6D case, for more/less dimension, number of list element must be modified
     '''
+    # Generate boundary list of each band
     bd_list = []
     for i in range(len(sc_lower_bd)):
         # Note upper_bd here already out of the 6D space, no needs to +1 in np.arange func
-        bd_list.append(np.arange(sc_lower_bd[i], sc_upper_bd[i], 1))
+        bd_list.append(list(np.arange(sc_lower_bd[i], sc_upper_bd[i], 1)))
+
+    # Generate origins and put -999 to lack band
+    xx = eval('np.meshgrid{}'.format(tuple([bd for bd in bd_list])))
+    XX = np.array([xx[i].flatten() for i in range(len(xx))])
     origin_list = []
-    xx = np.meshgrid(bd_list[0], bd_list[1], bd_list[2], bd_list[3], bd_list[4])
-    x0, x1, x2, x3, x4 = xx[0], xx[1], xx[2], xx[3], xx[4]
-    xx0, xx1, xx2, xx3, xx4 = x0.flatten(), x1.flatten(), x2.flatten(), x3.flatten(), x4.flatten()
-    for j in range(len(x0.flatten())):
-        new_origin = [xx0[j], xx1[j], xx2[j], xx3[j], xx4[j]]
+    for i in range(len(XX[0])):
+        new_origin = [XX[j, i] for j in range(len(XX))]
         new_origin.insert(sc_fixed_bd, 0)
         origin_list.append(np.array(new_origin))
+
+    # Just Backup Old Code ...
+    # xx = np.meshgrid(bd_list[0], bd_list[1], bd_list[2], bd_list[3], bd_list[4])
+    # x0, x1, x2, x3, x4 = xx[0], xx[1], xx[2], xx[3], xx[4]
+    # xx0, xx1, xx2, xx3, xx4 = x0.flatten(), x1.flatten(), x2.flatten(), x3.flatten(), x4.flatten()
+    # origin_list = []
+    # for j in range(len(x0.flatten())):
+        # new_origin = [xx0[j], xx1[j], xx2[j], xx3[j], xx4[j]]
+        # new_origin.insert(sc_fixed_bd, 0)
+        # origin_list.append(np.array(new_origin))
+
     return origin_list
 
 def generate_probe_line(origin, probe_vec, band_upper_bd):
@@ -59,27 +73,36 @@ def generate_probe_line(origin, probe_vec, band_upper_bd):
     probe_vec = np.array(probe_vec)
     if np.all(np.less_equal(probe_vec, np.zeros(len(band_upper_bd)))):
         probe_vec = -1 * probe_vec
+
     # Prevent infinite loops
     if np.any(np.less(probe_vec, np.zeros(len(band_upper_bd)))):
         exit('Wrong probe vector ...')
+
     # Larger than origin
     probe_line, pos, i = [], origin, 1
     while np.all(np.less(pos, band_upper_bd-0.5)):
         probe_line.append(pos)
         pos = origin + (i * probe_vec)
         i += 1
+
     # Round and store in array
     probe_round = np.rint(probe_line)
-    probe_line  = np.array(probe_round, dtype=int)
+    probe_line  = list(np.array(probe_round, dtype=int))
     return probe_line
 
-def get_gp_along_line(probe_line, gal_pos, gal_num):
+def get_gp_along_line(lack_ind_list, probe_line, gal_pos, gal_num):
     '''
     This is used to get galaxy probability along probe line
     '''
     gp_along_line = []
     for i in range(len(probe_line)):
-        probe_pos = probe_line[i]
+
+        # Assign -999 to lack band
+        probe_pos = list(probe_line[i])
+        for lack_ind in lack_ind_list:
+            probe_pos.insert(lack_ind, -999)
+        probe_pos = np.array(probe_pos)
+
         loc_id = find_pos_id_in_gal_pos(gal_pos, probe_pos)
         if (len(loc_id) == 1):
             num = float(gal_num[loc_id])
@@ -130,13 +153,14 @@ def find_gp_boundary(probe_line, gp_along_line):
         upper_gp_bound = [np.nan] * len(probe_line[0])
     return lower_gp_bound, upper_gp_bound
 
-def find_bd_of_diff_origins(index, len_origin, origin, probe_vec, band_upper_bd, gal_pos, gal_num, gp_lower_bd_list, gp_upper_bd_list):
+def find_bd_of_diff_origins(index, len_origin, origin, probe_vec, lack_ind_list, band_upper_bd, gal_pos, gal_num, gp_lower_bd_list, gp_upper_bd_list):
     '''
     This is to combine all above functions (for parallel computation)
     '''
     # Main calculation
+    print(index)
     probe_line    = generate_probe_line(origin, probe_vec, band_upper_bd)
-    gp_along_line = get_gp_along_line(probe_line, gal_pos, gal_num)
+    gp_along_line = get_gp_along_line(lack_ind_list, probe_line, gal_pos, gal_num)
     gp_lower_bd, gp_upper_bd = find_gp_boundary(probe_line, gp_along_line)
     if np.nan not in gp_lower_bd:
         gp_lower_bd_list.append(gp_lower_bd)
@@ -148,43 +172,83 @@ def find_bd_of_diff_origins(index, len_origin, origin, probe_vec, band_upper_bd,
 # Main Program
 #==========================================================
 if __name__ == '__main__':
-    s_start = time.time()
+    p_start = time.time()
+
+    # # Check inputs
+    # if len(argv) != 11:
+        # exit('\n\tError: Wrong Arguments\
+        # \n\tExample: [program] [dim] [cube size] [sigma] [bond] [ref-D] [band_inp] [fixed_band_id] [lower_bd] [upper_bd] [n_thread]\
+        # \n\t[dim]: dimension for smooth (for now only "6")\
+        # \n\t[cube size]: length of multi-d cube in magnitude unit\
+        # \n\t[sigma]: standard deviation for gaussian dist. in magnitude\
+        # \n\t[bond]: boundary radius of gaussian beam unit in cell\
+        # \n\t[ref-D]: reference dimension which to modulus other dimension to\
+        # \n\t[band_inp]: band used to do smooth in string e.g. 012345\
+        # \n\t[fixed_band_id]: index of band that fixed when calculating with different origins\
+        # \n\t[lower_bd]: bound of input bands except fixed one (unit:cell) e.g. "0,0,0,0,0" or "default"\
+        # \n\t[upper_bd]: bound of input bands except fixed one (unit:cell) e.g. "9,9,9,9,9" or "default"\
+        # \n\t[n_thread]: number of thread for parallel computation\n')
+    # # Input variables
+    # dim         = int(argv[1])       # Dimension of position vector
+    # cube        = float(argv[2])     # Beamsize for each cube
+    # sigma       = int(argv[3])       # STD for Gaussian Smooth
+    # bond        = int(argv[4])       # Bond for Gaussian Smooth
+    # refD        = int(argv[5])       # Reference Beam Dimension
+    # band_inp    = str(argv[6])       # Input band ids
+    # sc_fixed_bd = int(argv[7])       # Fixed band id
+    # n_thread    = int(argv[10])      # Number of threads to parallel computation
+    # posv_dir    = 'GPV_{:d}Dposvec_bin{:.1f}/'.format(dim, cube)
+    # out_prefix  = 'GPV_after_smooth_{:d}D_bin{:.1f}_sigma{:d}_bond{:d}_refD{:d}'.format(dim, cube, sigma, bond, refD)
+    # out_dir     = '{}/'.format(out_prefix)
 
     # Check inputs
-    if len(argv) != 11:
-        exit('\n\tError: Wrong Arguments\
-        \n\tExample: [program] [dim] [cube size] [sigma] [bond] [ref-D] [band_inp] [fixed_band_id] [lower_bd] [upper_bd] [n_thread]\
-        \n\t[dim]: dimension for smooth (for now only "6")\
-        \n\t[cube size]: length of multi-d cube in magnitude unit\
-        \n\t[sigma]: standard deviation for gaussian dist. in magnitude\
-        \n\t[bond]: boundary radius of gaussian beam unit in cell\
-        \n\t[ref-D]: reference dimension which to modulus other dimension to\
-        \n\t[band_inp]: band used to do smooth in string e.g. 012345\
-        \n\t[fixed_band_id]: index of band that fixed when calculating with different origins\
-        \n\t[lower_bd]: bound of input bands except fixed one (unit:cell) e.g. "0,0,0,0,0" or "default"\
-        \n\t[upper_bd]: bound of input bands except fixed one (unit:cell) e.g. "9,9,9,9,9" or "default"\
-        \n\t[n_thread]: number of thread for parallel computation\n')
+    parser = ArgumentParser(description='Find galaxy boundary in multi-D magnitude space', \
+                            epilog='For indice for input band and fixed band: J[0], IR1[1], IR2[2], IR3[3], IR4[4], MP1[5]')
+    parser.add_argument('dim', type=int, help='Dim of input position vector')
+    parser.add_argument('cube', type=float, help='Length of binning size (One decimal place)')
+    parser.add_argument('sigma', type=int, help='Standard deviation of gaussian smooth (unit: bin)')
+    parser.add_argument('bond', type=int, help='Cutoff radius of guassian smooth (unit: bin)')
+    parser.add_argument('refD', type=int, help='Reference dimension for gaussian smooth')
+    parser.add_argument('band_inp', type=str, help='Indice of bands to use (e.g 12345)')
+    parser.add_argument('sc_fixed_band', type=int, help='Index of band fixed to generate plane to find boundary')
+    parser.add_argument('-n_th', '--Number_of_Thread', dest='n_thread', default=10, help='Number of thread for parallel computation')
+    parser.add_argument('-posv_dir', '--Position_Vector_Directory', dest='posv_dir', \
+                        help='Directory that stores galaxy position vector and mutl-D space shape')
+    parser.add_argument('-out_dir', '--Output_Directory', dest='out_dir', help='Directory that stores output boundaries')
 
-    # Input variables
-    dim         = int(argv[1])       # Dimension of position vector
-    cube        = float(argv[2])     # Beamsize for each cube
-    sigma       = int(argv[3])       # STD for Gaussian Smooth
-    bond        = int(argv[4])       # Bond for Gaussian Smooth
-    refD        = int(argv[5])       # Reference Beam Dimension
-    band_inp    = str(argv[6])       # Input band ids
-    sc_fixed_bd = int(argv[7])       # Fixed band id
-    n_thread    = int(argv[10])      # Number of threads to parallel computation
-    posv_dir    = 'GPV_{:d}Dposvec_bin{:.1f}/'.format(dim, cube)
-    out_prefix  = 'GPV_after_smooth_{:d}D_bin{:.1f}_sigma{:d}_bond{:d}_refD{:d}'.format(dim, cube, sigma, bond, refD)
-    out_dir     = '{}/'.format(out_prefix)
+    # Load input from parser
+    args        = parser.parse_args()
+    dim         = args.dim
+    cube        = args.cube
+    sigma       = args.sigma
+    bond        = args.bond
+    refD        = args.refD
+    band_inp    = args.band_inp
+    sc_fixed_bd = args.sc_fixed_band
+    n_thread    = args.n_thread
+    posv_dir    = args.posv_dir
+    out_dir     = args.out_dir
+    if posv_dir is None:
+        posv_dir = 'GPV_{:d}Dposvec_bin{:.1f}/'.format(dim, cube)
+    if out_dir is None:
+        out_prefix  = 'GPV_after_smooth_{:d}D_bin{:.1f}_sigma{:d}_bond{:d}_refD{:d}'.format(dim, cube, sigma, bond, refD)
+        out_dir     = '{}/'.format(out_prefix)
 
     # Load arrays for calculations
-    probe_vec0    = [0] * (len(band_inp)-1); probe_vec0.insert(sc_fixed_bd, 1)
+    probe_vec0    = [0] * (len(band_inp))
     shape         = np.load(posv_dir + 'Shape.npy')
     band_upper_bd = np.array([int(shape[int(ind)]) for ind in band_inp])
     band_lower_bd = np.array([0 for ind in band_inp])
     sc_lower_bd   = [0 for i in range(len(band_inp)-1)]
     sc_upper_bd   = [shape[int(i)] for i in band_inp if int(i) != sc_fixed_bd]
+
+    # Generate lack band index list
+    all_ind_list  = [i for i in range(dim)]
+    inp_ind_list  = [int(band_inp[i]) for i in range(len(band_inp))]
+    lack_ind_list = []
+    for ind in all_ind_list:
+        if ind not in inp_ind_list:
+            lack_ind_list.append(ind)
 
     # Galaxy position vector and number (Remove pos with num < 1.0 to increase efficiency)
     load_time = time.time()
@@ -194,16 +258,18 @@ if __name__ == '__main__':
     gal_num = gal_num[gal_num >= 1.0]
 
     # Use different origins to find boundaries
-    origin_list = generate_6D_origins_on_plane(band_inp, sc_fixed_bd, sc_lower_bd, sc_upper_bd)
+    origin_list = generate_origins_on_plane(sc_fixed_bd, sc_lower_bd, sc_upper_bd)
     len_origin  = len(origin_list)
     print('\nFixed band id: {}\nOrigins lower bound: {}\nOrigins upper bound: {}\n# of origins: {:d}\n'.format(\
             sc_fixed_bd, sc_lower_bd, sc_upper_bd, len_origin))
+    p_end   = time.time()
 
     # Parallel Computing (Note: require='sharedmen' is essential for list parallel)
+    s_start = time.time()
     gp_lower_bd_list, gp_upper_bd_list = [], []
     Parallel(n_jobs=n_thread, require='sharedmem')\
             (delayed(find_bd_of_diff_origins)\
-            (i, len_origin, origin_list[i], probe_vec0, band_upper_bd, gal_pos, gal_num, gp_lower_bd_list, gp_upper_bd_list)\
+            (i, len_origin, origin_list[i], probe_vec0, lack_ind_list, band_upper_bd, gal_pos, gal_num, gp_lower_bd_list, gp_upper_bd_list)\
             for i in range(len_origin))
 
     # Non-Parallel method (Just for backup)
@@ -222,5 +288,6 @@ if __name__ == '__main__':
 
     # Print out result ...
     s_end   = time.time()
-    print('\nFinding {:d}D boundary took {:.3f} secs\
-           \nAverage time for each probe: {:.3f} secs\n'.format(dim, s_end - s_start, (s_end-s_start)/len(origin_list)))
+    print('\nWhole Finding {:d}D boundary took {:.3f} secs\
+           \nGenerate mult-d origins took {:.3f} secs\
+           \nAverage time for each probe: {:.3f} secs\n'.format(dim, s_end-p_start, p_end-p_start, (s_end-s_start)/len(origin_list)))
